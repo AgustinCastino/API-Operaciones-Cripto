a5e464f1c5bf101470600bfa41b165a6bb86836b
Agustín Castino <aguscastino01@gmail.com>
2024-08-20 00:35:08 -0300

    Se agrega middleware para cors

--- Cripto/app.js
+++ Cripto/app.js
@@ -1,9 +1,14 @@
 import express, { json } from 'express' // ES Modules
 import { criptoRouter } from './routes/criptoRouter.js'
- 
+
+//const cors = require('cors');
+import cors from 'cors';
+
+
 const app = express()
 // MiddleWare para juntar los datos de la req
 app.use(json()) // MiddleWare
+app.use(cors()); // Middleware
 
 // Desactiva info de la cabecera
 app.disable('x-powered-by') 
