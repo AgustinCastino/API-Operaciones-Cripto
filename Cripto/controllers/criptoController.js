166a39ad18e29e08ec4667336360da56d90973fa
AgustinCastino <aguscastino01@gmail.com>
2025-07-07 00:20:48 -0300

    Separacion de controllers para Cripto y Trade

--- Cripto/controllers/criptoController.js
+++ Cripto/controllers/criptoController.js
@@ -1,83 +1,36 @@
 import { criptoModel } from "../models/cripto.js"
-import { validarOperacion, validarOperacionParcial} from "../schema/operacion.js"
+import { validarCripto, validarCriptoParcial} from "../schema/cripto.js"
 
 
 export class criptoController {
 
     static async getAll(req, res) {
-        try{
-            const criptos = await criptoModel.getAll()
-            res.json(criptos)
-        }catch{
-            console.error('Error getAll', err)
-        }
+        return null
     }
 
-    static async getFilteredCripto(req, res){
-        try{
-            const cripto = req.params.cripto 
-            const filteredCripto = await criptoModel.getFilteredCripto(cripto);
-        
-            res.json(filteredCripto);
-        }catch{
-            console.error('Error en getFilteredCripto', err)
+    static async newCripto(req, res) {
+        cripto = req.body
+        cripto = {
+            ...req.body,
+            lastUpdate: new Date()
         }
-            
-    }
 
-    static async getCompras(req, res) {
         try{
-            const compras = await criptoModel.getCompras()
-            res.json(compras)
-        }catch{
-            console.error('Error en getCompras', err)
-        }
-    }
-
-    static async getVentas(req, res) {
-        try{
-            const ventas = await criptoModel.getVentas()
-            res.json(ventas)
-        }catch{
-            console.error('Error getVentas', err);
-            
-        }
-    }
-
-    static async crearOperacion(req, res) {
-        try{
-            const validacion = validarOperacion(req.body)
-    
-            if (!validacion.success){
+            const validatedCripto = validarCripto(cripto)
+            if (!validatedCripto.success){
                 return res.status(400).json({ error: JSON.parse(validacion.error.message) })
             }
+            const newCripto = await criptoModel.newCripto(cripto)
             
-            const operacionCreada = await criptoModel.crearOperacion(req.body)
-            
-            res.json(operacionCreada)
+            res.json(newCripto)
         }catch{
             console.error('Error crearOperacion', err);
         }
 
     }
 
-    static async actualizarOperacion(req, res) {
-        try{
-            const id = req.params.id
-    
-            const validacion = validarOperacionParcial(req.body)
-    
-            if (!validacion.success){
-                return res.status(400).json({ error: JSON.parse(validacion.error.message) })
-            }
-    
-            const operacionActualizada = await criptoModel.actualizarOperacion(id, validacion.data)
-    
-            res.json(operacionActualizada)
-        }catch{
-            console.error('Error actualizarOperacion', err);
-            
-        }
+    static async updateCripto(req, res) {
+        
     }
 
     
---
+++ Cripto/controllers/tradeController.js
@@ -0,0 +1,85 @@
+import { tradeModel } from "../models/trade.js"
+import { validarOperacion, validarOperacionParcial} from "../schema/operacion.js"
+
+
+export class tradeController {
+
+    static async getAll(req, res) {
+        try{
+            const criptos = await tradeModel.getAll()
+            res.json(criptos)
+        }catch{
+            console.error('Error getAll', err)
+        }
+    }
+
+    static async getFilteredCripto(req, res){
+        try{
+            const cripto = req.params.cripto 
+            const filteredCripto = await tradeModel.getFilteredCripto(cripto);
+        
+            res.json(filteredCripto);
+        }catch(err){
+            console.error('Error en getFilteredCripto', err)
+        }
+            
+    }
+
+    static async getCompras(req, res) {
+        console.log("ok")
+        try{
+            const compras = await tradeModel.getCompras()
+            res.json(compras)
+        }catch{
+            console.error('Error en getCompras', err)
+        }
+    }
+
+    static async getVentas(req, res) {
+        try{
+            const ventas = await tradeModel.getVentas()
+            res.json(ventas)
+        }catch{
+            console.error('Error getVentas', err);
+            
+        }
+    }
+
+    static async newTrade(req, res) {
+        try{
+            const validacion = validarOperacion(req.body)
+    
+            if (!validacion.success){
+                return res.status(400).json({ error: JSON.parse(validacion.error.message) })
+            }
+            
+            const operacionCreada = await tradeModel.newTrade(req.body)
+            
+            res.json(operacionCreada)
+        }catch{
+            console.error('Error crearOperacion', err);
+        }
+
+    }
+
+    static async updateTrade(req, res) {
+        try{
+            const id = req.params.id
+    
+            const validacion = validarOperacionParcial(req.body)
+    
+            if (!validacion.success){
+                return res.status(400).json({ error: JSON.parse(validacion.error.message) })
+            }
+    
+            const operacionActualizada = await tradeModel.updateTrade(id, validacion.data)
+    
+            res.json(operacionActualizada)
+        }catch{
+            console.error('Error actualizarOperacion', err);
+            
+        }
+    }
+
+    
+}
